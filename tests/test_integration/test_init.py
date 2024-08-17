e38b2315019b7030dcd9a62699b4e3140ae4456d
pushkarm029 <pushkarmishra029@gmail.com>
2024-08-18 00:20:21 +0530

    fix
    Signed-off-by: pushkarm029 <pushkarmishra029@gmail.com>

--- tests/test_integration/test_init.py
+++ tests/test_integration/test_init.py
@@ -5,7 +5,5 @@ from pathlib import Path
 
 def test_init_file_exists():
     """Check if the __init__.py file exists."""
-    path_to_init_file = (
-        Path(__file__).parents[2] / "cloud_telemetry" / "__init__.py"
-    )
+    path_to_init_file = Path(__file__).parents[2] / "cloud_telemetry" / "__init__.py"
     assert path_to_init_file.exists(), f"File {path_to_init_file} does not exist."
